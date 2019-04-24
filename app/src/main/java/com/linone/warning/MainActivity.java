b390e22abaaa69f44cd9f05870958b5fd37e0b02
黎晝 <boting87@gmail.com>
2019-04-24 10:50:18 +0800

    New

--- app/src/main/java/com/linone/warning/MainActivity.java
+++ app/src/main/java/com/linone/warning/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.IBinder;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private JsonObject[] Data = null;
     private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
-    private TextView warning_t,waterdepth,rain1,rain24,temp,vol;
-    private ImageView warning;
+    private TextView warning_t,waterdepth,rain1,rain24,temp,vol,date;
+    private ImageView warning,safe;
     private AudioManager audioManager;
     private Ringtone r;
     private Vibrator v;
@@ -48,10 +50,13 @@ public class MainActivity extends AppCompatActivity {
         rain24 = findViewById(R.id.rain_24h_d);
         temp = findViewById(R.id.temp_d);
         vol = findViewById(R.id.voltage_d);
+        date = findViewById(R.id.date);
+        safe = findViewById(R.id.safe);
         getData();
         audioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
         audioManager.setStreamVolume(AudioManager.STREAM_ALARM,audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM),AudioManager.FLAG_PLAY_SOUND);
         uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        r = RingtoneManager.getRingtone(MainActivity.this, uri);
         v=(Vibrator)getApplication().getSystemService(Service.VIBRATOR_SERVICE);
         warningimgblink();
     }
@@ -62,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 while(true){
                     try {
-                       Document document = Jsoup.connect("https://api.thingspeak.com/channels/699140/feeds.html?results=1").get();
+                       Document document = Jsoup.connect("https://api.thingspeak.com/channels/699140/feeds.html?results=1&timezone=Asia%2FTaipei").get();
                        Elements elements = document.select("body");
                        String S = elements.text();
                        com.google.gson.JsonObject j = gson.fromJson(S, com.google.gson.JsonObject.class);
@@ -74,17 +79,18 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            r = RingtoneManager.getRingtone(MainActivity.this, uri);
                             if(Integer.valueOf(Data[0].getAlarm())==1){
                                 r.stop();
                                 r.play();
                                 v.vibrate(9000);
                                 warning_t.setVisibility(View.VISIBLE);
+                                safe.setVisibility(View.INVISIBLE);
                                 blink = true;
                             }
                             else{
                                 r.stop();
                                 warning_t.setVisibility(View.INVISIBLE);
+                                safe.setVisibility(View.VISIBLE);
                                 blink = false;
                             }
                             waterdepth.setText(Data[0].getWaterLevel());
@@ -92,6 +98,9 @@ public class MainActivity extends AppCompatActivity {
                             rain24.setText(Data[0].getRainfall_24hour());
                             temp.setText(Data[0].getTemperature());
                             vol.setText(Data[0].getVoltage());
+                            String[] s = Data[0].getCreated_at().split("T");
+                            s[1] = s[1].split("\\+")[0];
+                            date.setText(s[0]+"  "+s[1]);
                         }
                     });
                     try{
